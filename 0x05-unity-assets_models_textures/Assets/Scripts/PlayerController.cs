62bfd66114ab263cb1b8889d5a2c5a1e96ead50a
Falendrac <leger_franck@protonmail.com>
2022-09-13 11:33:11 +0200

    Add documentary and delete debug log.

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Handle the control for the player
+/// Like ASDW and Jump
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
+    /// The force of the jump when the player use space bar
     private float _jumpForce = 2.0f;
+    /// Speed of the player
     private float _speed = 10f;
+    /// The default gravity to apply in the jump
     private float gravityValue = -9.81f;
+    /// Vector to apply the force of the jump to the player
     private Vector3 _velocity;
+    /// The controller of playe to apply movement and jump
     private CharacterController controller;
+    /// The rigidbody for the mass
     private Rigidbody _rb;
 
     // Start is called before the first frame update
@@ -31,7 +41,6 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetButtonDown("Jump"))
         {
-            Debug.Log("Bite");
             _velocity.y = Mathf.Sqrt(_jumpForce * -3.0f * gravityValue);
         }
 
