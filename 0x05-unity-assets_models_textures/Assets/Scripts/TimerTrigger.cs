018e300f1b63ee312d6463e781d93a507b1f0e83
Falendrac <leger_franck@protonmail.com>
2022-09-19 09:57:10 +0200

    Handle the winflag condition and add doc

--- 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>Triger the timer when player move</summary>
 public class TimerTrigger : MonoBehaviour
 {
+    // Get the player gameobject to set timer to false
     public GameObject playerTimer;
 
+    // When the player move, enable the timer
     void OnTriggerExit(Collider other)
     {
         if (other.name == "Player")
