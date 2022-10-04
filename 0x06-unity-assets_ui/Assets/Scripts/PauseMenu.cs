a0f5938ae736fad14e169d1f8e72e364d71ff154
Falendrac <leger_franck@protonmail.com>
2022-10-04 11:33:33 +0200

    Handle all functionnality of pausemenu

--- 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Handle all functionnality of pausemenu
+/// </summary>
 public class PauseMenu : MonoBehaviour
 {
+    /// <summary>Get the GameObject pauseCanvas to setactive</summary>
     public GameObject pauseCanvas;
 
+    /// <summary>
+    /// Activate the pause menu when the player press ESC
+    /// </summary>
     public void Pause()
     {
         pauseCanvas.SetActive(true);
@@ -13,11 +21,40 @@ public class PauseMenu : MonoBehaviour
         Cursor.visible = true;
     }
 
+    /// <summary>
+    /// Resume the game when the player press ESC or the resume button
+    /// </summary>
     public void Resume()
     {
         pauseCanvas.SetActive(false);
         Time.timeScale = 1;
         Cursor.visible = false;
+    }
+
+    /// <summary>
+    /// Restart the current level
+    /// </summary>
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    /// <summary>
+    /// Load the MainMenu
+    /// </summary>
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    /// <summary>
+    /// Load the options
+    /// </summary>
+    public void Options()
+    {
+        Time.timeScale = 1;
     }
 
     // Start is called before the first frame update
