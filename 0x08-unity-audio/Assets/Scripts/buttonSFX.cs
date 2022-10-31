3b9ff5f9f7d9211de0670511579473e6b0a41ac5
Falendrac <leger_franck@protonmail.com>
2022-10-31 15:45:20 +0100

    Apply the sound effect when click on button

--- 0x08-unity-audio/Assets/Scripts/buttonSFX.cs
+++ 0x08-unity-audio/Assets/Scripts/buttonSFX.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class buttonSFX : MonoBehaviour
+public class ButtonSFX : MonoBehaviour
 {
-    public AudioSource myFx;
-    public AudioClip hoverFx;
+    public AudioSource hoverFx;
+    public AudioSource clickFx;
 
     public void HoverSound()
     {
-        myFx.PlayOneShot(hoverFx);
+        hoverFx.Play();
+    }
+
+    public void ClickSound()
+    {
+        clickFx.Play();
     }
 }
